21649d42488883beb58d709f4a9d1a05c75d2900
duk1edev <duk1e.ptc.ua@yandex.ru>
2020-04-29 12:24:08 +0300

    added new route

--- 11_flask/main.py
+++ 11_flask/main.py
@@ -7,6 +7,9 @@ app = Flask('__name__')
 def home():
     return 'Hello first Flask App'
 
+@app.route('/duk1e')
+def duk1e():
+    return ('<h2>Hello duk1e</h2>' + '<h5>Olololo</h5>')
 
 if __name__ == '__main__':
     app.run()
